a2e89d12444e42ab50cc7661a799b1bbbd337c2d
Dominik Gryžbon <67821264+h3pereu@users.noreply.github.com>
2026-04-13 15:27:05 +0200

    fix(carplay): wrap RCT_EXTERN_MODULE in @interface/@end in CarPlayBridge.m

--- ios/CarPlayBridge.m
+++ ios/CarPlayBridge.m
@@ -1,5 +1,4 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(CarPlayBridge, RCTEventEmitter)
-@end
+RCT_EXTERN_MODULE(CarPlayBridge, RCTEventEmitter)
